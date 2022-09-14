7fbc6f67feda6bcbfc3c3ca033c917ac59bce96c
navdeepkaur2 <navdeepkaurgumti9888@gmail.com>
2022-09-14 17:50:03 -0500

    add lab1 submittion

--- SD-340-LABS/SD-340-LABS/Program.cs
+++ SD-340-LABS/SD-340-LABS/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System.Text;
+
+string ConcatinateString(string wordArg, int numArg, string s)
+{
+    string string1 = s;
+    for(int i = 1; i < numArg; i++)
+    {
+        s += string1;
+    }
+    return string1;
+}
+
+
+string myString = "go away";
+int myInt = 4;
+
+myDelegate newDelegate = new myDelegate(ConcatinateString);
+
+Console.WriteLine(newDelegate(myString,myInt));
+delegate string myDelegate  (string wordArg, int numArg);
